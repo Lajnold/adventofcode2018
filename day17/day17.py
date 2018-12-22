6b4314d631ca75f9e2709017f7d6f2775fcdffdb
Linus Nilsson <lajnold gmail com>
2018-12-22 03:26:49 +0100

    Started day 17

---
+++ day17/day17.py
@@ -0,0 +1,157 @@
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def up(self):
+        return Pos(self.x, self.y - 1)
+
+    def down(self):
+        return Pos(self.x, self.y + 1)
+
+    def left(self):
+        return Pos(self.x - 1, self.y)
+
+    def right(self):
+        return Pos(self.x + 1, self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __str__(self):
+        return f'Pos({self.x}, {self.y})'
+
+    def __repr__(self):
+        return str(self)
+
+
+def parse_range(r):
+    # 533
+    # 456..458
+    sp = r.split('..')
+    if len(sp) == 1:
+        return [int(sp[0])]
+    else:
+        return list(range(int(sp[0]), int(sp[1]) + 1))
+
+
+def parse_input(lines):
+    clays = []
+    for line in lines:
+        # x=533, y=1429..1441
+        # y=1785, x=456..458
+        sp = line.split(', ')
+        x_part = sp[0][2:] if sp[0][0] == 'x' else sp[1][2:]
+        y_part = sp[0][2:] if sp[0][0] == 'y' else sp[1][2:]
+        for x in parse_range(x_part):
+            for y in parse_range(y_part):
+                clays.append(Pos(x, y))
+    
+    return clays
+
+
+def load_input():
+    with open('input.txt') as f:
+        lines = f.read().strip().splitlines()
+        return parse_input(lines)
+
+
+def write_to_file(clay, water):
+    min_x = min(clay.x for clay in clay)
+    max_x = max(clay.x for clay in clay)
+    min_y = min(clay.y for clay in clay)
+    max_y = max(clay.y for clay in clay)
+
+    with open('output.txt', 'w') as f:
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                here = Pos(x, y)
+                if here in clay:
+                    f.write('#')
+                elif here in water:
+                    f.write('~')
+                else:
+                    f.write('.')
+
+            f.write('\n')
+
+
+
+def part1():
+    clay = set(load_input())
+    min_y = min(clay.y for clay in clay)
+    max_y = max(clay.y for clay in clay)
+
+    sources = [Pos(500, 0)]
+    water = set()
+
+    while sources:
+        source = sources[0]
+        del sources[0]
+
+        expander = None
+        source = source.down()
+        while source and source.y <= max_y:
+            if source in clay or source in water:
+                expander = source.up()
+                source = None
+            else:
+                if source.y >= min_y:
+                    water.add(source)
+                source = source.down()
+
+        while expander:
+            left_clay = False
+            left_expander = expander.left()
+            while left_expander:
+                if left_expander in clay:
+                    left_clay = True
+                    left_expander = None
+                elif left_expander in water:
+                    left_expander = None
+                else:
+                    if left_expander.y >= min_y:
+                        water.add(left_expander)
+
+                    if left_expander.down() in clay or left_expander.down() in water:
+                        left_expander = left_expander.left()
+                    else:
+                        sources.append(left_expander)
+                        left_expander = None
+
+            right_clay = False
+            right_expander = expander.right()
+            while right_expander:
+                if right_expander in clay:
+                    right_clay = True
+                    right_expander = None
+                elif right_expander in water:
+                    right_expander = None
+                else:
+                    if right_expander.y >= min_y:
+                        water.add(right_expander)
+
+                    if right_expander.down() in clay or right_expander.down() in water:
+                        right_expander = right_expander.right()
+                    else:
+                        sources.append(right_expander)
+                        right_expander = None
+
+            if left_clay and right_clay:
+                expander = expander.up()
+            else:
+                expander = None
+
+    write_to_file(clay, water)
+    print(f'Part 1: {len(water)}')
+
+
+def part2():
+    pass
+
+
+part1()
+part2()
